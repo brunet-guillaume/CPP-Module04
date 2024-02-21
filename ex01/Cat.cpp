c9f0e41cb917c2815b25e813cad15c51645f7510
Guillaume Brunet <gbrunet@student.42.fr>
2024-02-21 17:15:13 +0100

    Copies aren't deep if you copy the same classes. Need to check everywhere in this module, and probably in other modules

--- ex01/Cat.cpp
+++ ex01/Cat.cpp
@@ -6,7 +6,7 @@
 /*   By: gbrunet <gbrunet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 14:37:11 by gbrunet           #+#    #+#             */
-/*   Updated: 2024/02/15 14:32:22 by gbrunet          ###   ########.fr       */
+/*   Updated: 2024/02/21 17:13:55 by gbrunet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,9 @@ Cat	&Cat::operator=(const Cat &rhs) {
 	std::cout << "\e[0;35mCat: Copy assignment operator called\e[0m" << std::endl;
 	if (this == &rhs)
 		return (*this);
+	// ici c'est pas bon. (au dessus je veux dire). car si tu duplique le meme
+	// truc, il ne va pas dup les brains.
+	// you need to correct this
 	this->type = rhs.type;
 	this->brain = new Brain(*rhs.brain);
 	return (*this);
